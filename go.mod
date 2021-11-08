bdd14a1164704432aadaad430f38bcf40691849a
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-11-08 21:11:36 +0300

    Add notifications and command to open story in browser

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tsivinsky/hackernews-feed
 
 go 1.17
+
+require github.com/0xAX/notificator v0.0.0-20210731104411-c42e3d4a43ee
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"strings"
 	"time"
+
+	"github.com/0xAX/notificator"
 )
 
 type Story struct {
@@ -56,6 +61,47 @@ func main() {
 		os.Exit(1)
 	}
 
+	storyChan := make(chan Story)
+	cmdChan := make(chan string)
+
+	go listenForStories(apiUrl, storyChan)
+	go listenForCommands(cmdChan)
+
+	var story Story
+	for {
+		select {
+		case story = <-storyChan:
+			printStory(story)
+		case cmd := <-cmdChan:
+			if cmd == "open" || cmd == "o" {
+				err := openInBrowser(story.Url)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
+	}
+}
+
+func listenForCommands(c chan string) {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		s, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		s = strings.TrimSpace(s)
+
+		c <- s
+	}
+}
+
+func listenForStories(apiUrl string, c chan Story) {
+	notify := notificator.New(notificator.Options{
+		AppName: "hacker-news-feed",
+	})
+
 	var latestStoryId int
 
 	for {
@@ -79,24 +125,46 @@ func main() {
 			continue
 		}
 
-		latestStoryId = story.Id
-
-		fmt.Printf("\"")
-		fmt.Print(string(YELLOW_COLOR))
-		fmt.Printf("%s", story.Title)
-		fmt.Print(string(RESET_COLOR))
-		fmt.Print("\"")
-		fmt.Printf(" by %s\n", story.By)
-
-		var url string
-		if story.Url != "" {
-			url = story.Url
-		} else {
-			url = fmt.Sprintf("https://news.ycombinator.com/item?id=%d", story.Id)
+		if latestStoryId > 0 {
+			notify.Push("New Hacker Story", story.Title, "", notificator.UR_NORMAL)
 		}
 
-		fmt.Printf("  - %s\n", url)
+		latestStoryId = story.Id
+
+		c <- story
 	}
+}
+
+func openInBrowser(url string) error {
+	cmd := exec.Command("xdg-open", url)
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func printStory(story Story) {
+	var storyInString string
+
+	var url string
+	if story.Url != "" {
+		url = story.Url
+	} else {
+		url = fmt.Sprintf("https://news.ycombinator.com/item?id=%d", story.Id)
+		story.Title = fmt.Sprintf("[Ask] %s", story.Title)
+	}
+
+	storyInString += "\""
+	storyInString += string(YELLOW_COLOR)
+	storyInString += fmt.Sprintf("%s", story.Title)
+	storyInString += string(RESET_COLOR)
+	storyInString += "\""
+	storyInString += fmt.Sprintf(" by %s\n", story.By)
+	storyInString += fmt.Sprintf("  - %s\n", url)
+
+	fmt.Print(storyInString)
 }
 
 func getNewStories(apiUrl string) ([]int, error) {
